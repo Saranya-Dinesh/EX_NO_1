19a347f881c827ab5ff6ad5093f83fb23a1fc4f5
Saranya-Dinesh <102136479+Saranya-Dinesh@users.noreply.github.com>
2022-03-22 11:44:41 +0530

    Update CustomerManagement.java

--- CustomerManagement.java
+++ CustomerManagement.java
@@ -1,11 +1,6 @@
 package com.kiot.customermanagement;
 
 import org.kiot.customeroperations.CustomerSupport;
-/**
- *	CustomerManagement class performs customer operations
- *	@version 04/03/2022
- *  @author praveen kandhan
- */
 public class CustomerManagement  {
 	public static void main(String[] args)  {
 		// TODO Auto-generated method stub
